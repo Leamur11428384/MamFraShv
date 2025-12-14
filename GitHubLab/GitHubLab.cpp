6656f7518f5c97d5be6e4ae8aa366491c4928eb3
Amir <mametov.amir.0105@gmail.com>
2025-12-14 23:45:23 +0500

    Add program skelet

--- GitHubLab/GitHubLab.cpp
+++ GitHubLab/GitHubLab.cpp
@@ -1,6 +1,21 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+vector<string> readlines(const string& filename);
+void printlines(const vector<string>& lines);
+void writelines(const vector<string>& lines, const string& filename);
+
+int main(){
+
+    vector<string> lines;
+
+    lines = readlines("input.txt");
+    printlines(lines);
+    writelines(lines, "output.txt");
+    
+    return 0;
+
 }
